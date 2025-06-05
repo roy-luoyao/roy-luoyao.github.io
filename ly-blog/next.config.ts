e580b86ae9563da933f4b03f76219f9bbc12428c
yao.luo <yao.luo@maplequad.com>
2025-06-05 16:37:45 +0800

    init

--- ly-blog/next.config.ts
+++ ly-blog/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: 'export',
+  // 如果你的项目不是部署在根目录，需加 basePath
+  basePath: '/ly-blog',
+  // 如果你用 app router，建议加 trailingSlash
+  trailingSlash: true,
 };
 
 export default nextConfig;
