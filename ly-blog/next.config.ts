1af5e838a0045763a43361fc7ae9e24321c05d5e
yao.luo <yao.luo@maplequad.com>
2025-06-05 17:06:36 +0800

    feat: update home page

--- ly-blog/next.config.ts
+++ ly-blog/next.config.ts
@@ -4,9 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
     output: 'export',
   // 如果你的项目不是部署在根目录，需加 basePath
-  basePath: '/ly-blog',
+  // basePath: '/ly-blog',
   // 如果你用 app router，建议加 trailingSlash
   trailingSlash: true,
 };
-
 export default nextConfig;
